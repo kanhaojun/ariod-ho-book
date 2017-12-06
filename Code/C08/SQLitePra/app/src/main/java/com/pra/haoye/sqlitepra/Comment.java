9f27835d62fc3bf0c0c39dc75c1afcbdae273a00
kancheng <zxdfgcv@gmail.com>
2017-12-06 11:27:41 +0800

    Upload C08 Code SQLitePra

---
+++ Code/C08/SQLitePra/app/src/main/java/com/pra/haoye/sqlitepra/Comment.java
@@ -0,0 +1,32 @@
+package com.pra.haoye.sqlitepra;
+
+/**
+ * Created by kancheng on 2017/12/6.
+ */
+
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
